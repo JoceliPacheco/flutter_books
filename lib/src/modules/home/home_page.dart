decbaa1b5bbea59b67a4c3c60e055ce7db403e73
Joceli Pacheco <joceli.devops@gmail.com>
2023-06-15 18:36:57 -0300

    home page

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -5,8 +5,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return const Scaffold(
+      body: Center(
+        child: Text('HOME'),
+      ),
     );
   }
 }
